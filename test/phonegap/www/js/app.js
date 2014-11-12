8b189dd715201c1a77234561406e5e717713fde4
Justin Cosentino <justintcosentino@gmail.com>
2014-11-12 13:52:00 -0500

    Fixed accelerometer data

--- test/phonegap/www/js/app.js
+++ test/phonegap/www/js/app.js
@@ -2,7 +2,7 @@
   'use strict';
   var module = angular.module('app', ['onsen']);
 
-  module.controller('AppController', function($scope, $surveys) {
+  module.controller('AppController', function($scope) {
     $scope.doSomething = function() {
       setTimeout(function() {
         alert('Soon you can use this sick new feature!');
@@ -89,16 +89,3 @@
       return surveys;
   });
 })();
-
-// function onSuccess(acceleration) {
-//   alert('Acceleration X: ' + acceleration.x + '\n' +
-//         'Acceleration Y: ' + acceleration.y + '\n' +
-//         'Acceleration Z: ' + acceleration.z + '\n' +
-//         'Timestamp: '      + acceleration.timestamp + '\n');
-// };
-
-// function onError() {
-//     alert('onError!');
-// };
-
-// navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
