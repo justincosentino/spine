c6a21f89c13b4f9d4707c3637d2cb7c012825743
Emanuel Schorsch <emanuelschorsch@dhcp-130-58-192-109.swarthmore.edu>
2014-11-18 11:21:55 -0500

    added bluetooth plugin make sure to add the plugin: com.randdusing.bluetoothle

--- test/phonegap/www/js/app.js
+++ test/phonegap/www/js/app.js
@@ -2,57 +2,6 @@
   'use strict';
   var module = angular.module('app', ['onsen']);
 
-  module.controller('AppController', function($scope) {
-    $scope.doSomething = function() {
-      setTimeout(function() {
-        alert('Soon you can use this sick new feature!');
-      }, 100);
-    };
-
-    document.addEventListener('deviceready', onDeviceReady, false);
-
-    var onSuccess = function(acceleration) {
-      if (Math.abs(acceleration.x) > 10 ||
-          Math.abs(acceleration.y) > 10 ||
-          Math.abs(acceleration.z) > 10) {
-            var newSurvey = { 
-              title: acceleration.timestamp,
-              desc: 'Triggered by shaking the device.',
-              label: '[General Health]',
-              questions: [ 
-                          {
-                            type: 'radio',
-                            question: 'What is you favourite CS course?',
-                            options: ['CS91','CS21','CS97']
-                          }, 
-                          {
-                            type: 'text',
-                            question: 'Is this a super cool project?'
-
-                          },
-                          {
-                            type: 'range',
-                            question: 'Drag the thing to do the stuff based on your feelings:'
-                          }
-              ]
-            };
-            alert("WE ARE ACCELERATING WEEEEEEE");
-      }
-    };
-
-    function onError() {
-        alert('onError!');
-    }
-
-    var options = { frequency: 3000 }; 
-
-    function onDeviceReady() {  
-      console.log(navigator.accelerometer);
-      var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
-    }
-
-  });
-
   module.controller('DetailController', function($scope, $surveys) {
     $scope.item = $surveys.selectedItem;
   });
@@ -67,6 +16,7 @@
     };
 
     $scope.addSurvey = function() {
+      //alert("addding survey");
       setTimeout(function() {
         $scope.items = [
           { 
@@ -123,6 +73,87 @@
     };
 
     $scope.addSurvey();
+
+  });
+
+  module.controller('AppController', function($scope) {
+    $scope.doSomething = function() {
+      setTimeout(function() {
+        alert('Soon you can use this sick new feature!');
+      }, 100);
+    };
+
+    document.addEventListener('deviceready', onDeviceReady, false);
+
+    var onSuccess = function(acceleration) {
+      if (Math.abs(acceleration.x) > 8 ||
+          Math.abs(acceleration.y) > 8 ||
+          Math.abs(acceleration.z) > 10) {
+            var newSurvey = { 
+              title: acceleration.timestamp,
+              desc: 'Triggered by shaking the device.',
+              label: '[General Health]',
+              questions: [ 
+                          {
+                            type: 'radio',
+                            question: 'What is you favourite CS course?',
+                            options: ['CS91','CS21','CS97']
+                          }, 
+                          {
+                            type: 'text',
+                            question: 'Is this a super cool project?'
+
+                          },
+                          {
+                            type: 'range',
+                            question: 'Drag the thing to do the stuff based on your feelings:'
+                          }
+              ]
+            };
+            alert(Object.keys(this));
+            //this.addSurvey(newSurvey);
+            alert("WE ARE ACCELERATING WEEEEEEE");
+      }
+    };
+    var temp = onSuccess.bind($scope);
+
+    function onError() {
+        alert('onError!');
+    }
+
+    var options = { frequency: 3000 }; 
+                    
+    var bluetoothSuccess = function(error){
+        alert("in success");
+    }
+                
+    var bluetoothError = function(error){
+        alert("in error "+error.error);
+        alert("messg: "+ error.message);
+    }
+                    
+    var startScanSuccessCallback = function(success){
+        alert("2in success: "+success.status);
+        alert("success: "+Object.keys(success));
+        for(var obj in success){
+            alert("key: "+obj+ " value: "+success[obj]);
+        }
+    }
+                    
+    var startScanErrorCallback = function(error){
+        alert("2in error "+error.error);
+        alert("messg: "+ error.message);
+    }
+
+    function onDeviceReady() {  
+      console.log(navigator.accelerometer);
+      var watchID = navigator.accelerometer.watchAcceleration(temp, onError, options);
+      bluetoothle.initialize(bluetoothSuccess, bluetoothError);
+      bluetoothle.startScan(startScanSuccessCallback, startScanErrorCallback, []);
+      /*bluetoothle.stopScan(stopScanSuccessCallback, stopScanErrorCallback);
+      bluetoothle.connect(connectSuccessCallback, connectErrorCallback, params);
+      bluetoothle.read(readSuccessCallback, readErrorCallback, params);*/
+    }
 
   });
 
